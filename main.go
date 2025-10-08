d292d51c58fa9fc5b6567a728f3740cf56f0bf97
iwanttobecomeabillionare <berlianbimaseto@gmail.com>
2025-10-08 04:59:41 +0000

    auto commit

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+
+	"github.com/charmbracelet/keygen"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+	"github.com/go-playground/validator/v10"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("jsjdhdh")
+	kp, err := keygen.New("awesome", keygen.WithPassphrase("halo"), keygen.WithKeyType(keygen.Ed25519))
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+
+	server, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort("localhost", "7000")),
+		wish.WithHostKeyPEM(kp.RawPrivateKey()),
+		wish.WithMiddleware(
+			bubbletea.Middleware(WishForm),
+			activeterm.Middleware(),
+			logging.Middleware(),
+		),
+	)
+
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Error(err.Error())
+		return
+	}
 }
